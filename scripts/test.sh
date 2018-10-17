736e1b7d74526381a3013d53d7ab4c58d3a594bf
Joel Podrebarac <joelpodrebar@gmail.com>
2018-10-17 11:41:15 -0400

    Setup docker and jenkins build

---
+++ scripts/test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export branch_name="${BRANCH_NAME:-local}"
+export image_name="ajoelpod/mock-json-server"
+docker run --detach=false -e --rm $image_name:$branch_name npm run lint
