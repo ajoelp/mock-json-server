47ba9ee49f74ee64b3e776fb05c5a14512e1bd8d
Joel Podrebarac <joelpodrebar@gmail.com>
2018-10-17 11:44:28 -0400

    Test failed build

--- src/client/index.js
+++ src/client/index.js
@@ -12,12 +12,12 @@ module.exports = function() {
         default: 8000
       }
     })
-    .help("help")
-    .alias("help", "h")
-    .version(pkg.version)
-    .alias("version", "v")
-    .example("$0 db.json", "")
-    .require(1, "Missing <source> argument").argv;
+.help("help")
+.alias("help", "h")
+.version(pkg.version)
+.alias("version", "v")
+.example("$0 db.json", "")
+.require(1, "Missing <source> argument").argv;
 
   run(argv);
 };
