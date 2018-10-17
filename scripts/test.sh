6afaf0d6e9649e2d7e9b5d13637b2a34a64af4be
Joel Podrebarac <joelpodrebar@gmail.com>
2018-10-17 12:13:16 -0400

    Add Jest for tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,3 +2,4 @@
 export branch_name="${BRANCH_NAME:-local}"
 export image_name="ajoelpod/mock-json-server"
 docker run --detach=false -e --rm $image_name:$branch_name npm run lint
+docker run --detach=false -e --rm $image_name:$branch_name npm run test
