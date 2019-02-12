0ef7fcdc580108ffbaab594153dd4394958b0f95
Joel Podrebarac <joelpodrebar@gmail.com>
2019-02-11 21:14:21 -0500

    Initial rewrite commit

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:8.12
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
-COPY . .
-EXPOSE 8000
-CMD [ "./bin/index.js", "/usr/src/app/data.json" ]
--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export branch_name="${BRANCH_NAME:-local}"
-export image_name="ajoelpod/mock-json-server"
-docker build -f Dockerfile -t $image_name:$branch_name .
